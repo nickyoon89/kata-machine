604567be835e2db7f32aadf4f437d11907cac69a
Nick Yoon <nickyoon89@gmail.com>
2026-03-12 20:12:20 -0400

    Search Done
    Binary Search, Linear Search and Exercise

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,2 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump_amount = Math.floor(Math.sqrt(breaks.length));
+    let i = jump_amount;
+
+    for(; i < breaks.length; i+=jump_amount){
+        if(breaks[i]) break;
+    }
+
+    let j = i - jump_amount;
+    //for(; j < Math.min(i, breaks.length); j++){
+    for(; j < i && j < breaks.length; j++){
+        if(breaks[j]) return j;
+    }
+    return -1;
 }
